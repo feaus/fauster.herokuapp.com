c0cbc55e8e6d9998444eefb01bb507f79d3a822b
Fede Auster <federicoauster@gmail.com>
2020-08-20 14:10:38 -0300

    Debug

--- my_new_site/settings.py
+++ my_new_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get("DEBUG"))
+DEBUG = (os.environ.get("DEBUG") == "True")
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fauster.herokuapp.com']
 
